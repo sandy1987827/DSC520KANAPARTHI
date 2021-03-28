46e1df8dd3e71a182c9b9c5d6a7783f0231d3011
sandy1987827 <75550979+sandy1987827@users.noreply.github.com>
2021-03-27 19:13:14 -0500

    Week2_VK

---
+++ Week2/week2_Assignment1_KanaparthiVenkata.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# Assignment: Week2, ASSIGNMENT 1
+# Name: Kanaparthi, Venkata
+# Date: 2021-03-27
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print('Hello World! I wonder why that is always the default coding text to start with')
+
+# Add two numbers together
+x=1+2
+print(x)
+
+# Subtract a number from another number
+x=2-1
+print(x)
+
+# Multiply two numbers
+x=2*3
+print(x)
+
+# Divide between two numbers
+x=6/3
+print(x)
+
+# Concatenate two strings together (any words)
+x='Hello'+' '+'World'
+print(x)
+
+# Create a list of 4 items (can be strings, numbers, both)
+listEx=[1,2,3,4]
+print(listEx)
+
+# Append an item to your list (again, can be a string, number)
+listEx.append(5)
+print(listEx)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+tupleEx=('Hello','World','Week','Two')
+print(tupleEx)
+
+
+# In[ ]:
+
+
+
+
